e50f63661b4468fc12b2e8783a5442f946fb327e
kpfister <kpfister@me.com>
2016-06-01 16:11:02 -0600

    Implement Model
    Completed the Implement Model section

--- Post/Post.swift
+++ Post/Post.swift
@@ -8,3 +8,38 @@
 
 import Foundation
 
+class Post {
+    
+    private let kUsername = "username"
+    private let kText = "text"
+    private let kTimestamp = "timestamp"
+    private let kIdentifier = "identifier"
+    
+    
+    let username: String
+    let text: String
+    let timestamp: NSTimeInterval
+    let identifier: NSUUID
+    
+    init(username: String, text: String, timestamp: NSTimeInterval? = NSDate().timeIntervalSince1970 , identifier: NSUUID? = NSUUID()) {
+        
+        self.username = username
+        self.text = text
+        self.timestamp = timestamp!
+        self.identifier = identifier!
+        
+    }
+    
+    init?(dictionary: [String: AnyObject]) {
+        guard let username = dictionary[kUsername] as? String,
+        text = dictionary[kText] as? String,
+        timestamp = dictionary[kTimestamp] as? NSTimeInterval,
+        identifier = dictionary[kIdentifier] as? String,
+        uniqueIdentifier = NSUUID(UUIDString: identifier)
+        else { return nil }
+        self.username = username
+        self.text = text
+        self.timestamp = timestamp
+        self.identifier = uniqueIdentifier
+    }
+}
