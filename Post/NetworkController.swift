7280c65d7ff095482e20c356a4735e3478cd6415
kpfister <kpfister@me.com>
2016-06-01 15:38:10 -0600

    Network Controller
    Completed the Network Controller section

--- Post/NetworkController.swift
+++ Post/NetworkController.swift
@@ -9,39 +9,37 @@
 import Foundation
 
 class NetworkController {
-    
+    /* Write a String typed enum called HTTPMethod. You will use this enum to classify our HTTP requests as GET, PUT, POST, PATCH, or DELETE requests. Add cases for each.
+     example: case Get = "GET"*/
     enum HTTPMethod: String {
         case Get = "GET"
         case Put = "PUT"
         case Post = "POST"
-        case Patch = "PATCH"
-        case Delete = "DELETE"
+        case Patch = "Catch"
+        case Delete = "Delete"
+        
     }
     
-    static func performRequestForURL(url: NSURL, httpMethod: HTTPMethod, urlParameters: [String: String]? = nil, body: NSData? = nil, completion:((data: NSData?, error: NSError?) -> Void)?) {
+    // Network Controller is what is communicating between our app and the API
+    static func performRequestForURL(url: NSURL, httpMethod: HTTPMethod, urlParameters: [String:String]? = nil, body: NSData? = nil, completion:((data: NSData?, error: NSError?) -> Void)?) {
         
         let requestURL = urlFromURLParameters(url, urlParameters: urlParameters)
-        
         let request = NSMutableURLRequest(URL: requestURL)
         request.HTTPMethod = httpMethod.rawValue
         request.HTTPBody = body
         
         let session = NSURLSession.sharedSession()
-        
         let dataTask = session.dataTaskWithRequest(request) { (data, response, error) in
-            
             if let completion = completion {
                 completion(data: data, error: error)
             }
         }
-        
         dataTask.resume()
     }
     
+    
     static func urlFromURLParameters(url: NSURL, urlParameters: [String: String]?) -> NSURL {
-        
         let components = NSURLComponents(URL: url, resolvingAgainstBaseURL: true)
-        
         components?.queryItems = urlParameters?.flatMap({NSURLQueryItem(name: $0.0, value: $0.1)})
         
         if let url = components?.URL {
