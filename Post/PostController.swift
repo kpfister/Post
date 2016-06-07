d9982918d62ce357e04fbe35a7bc6d231dc04728
kpfister <kpfister@me.com>
2016-06-06 18:05:48 -0600

    t

--- Post/PostController.swift
+++ Post/PostController.swift
@@ -10,22 +10,40 @@ import Foundation
 
 class PostController {
     
-    weak var delegate = PostControllerDelegate?()
+    
     
     
     static let baseURL = NSURL(string: "https://devmtn-post.firebaseio.com/posts")
-    let endpoint = baseURL?.URLByAppendingPathExtension("json")
+    static let endpoint = baseURL?.URLByAppendingPathExtension("json")
     
     
-    var posts: [Post] = [] // I think this will change
+    weak var delegate = PostControllerDelegate?()
+    
+    var posts: [Post] = [] {
+        didSet {
+            delegate?.postsUpdated(posts)
+        }
+    }
     
     init() {
         fetchPosts()
-        
+    }
+    // Mark Create post
+    func addPost(username: String, text: String) {
+        let post = Post.init(username: username, text: text)
+        guard let requestURL = PostController.endpoint else {
+            fatalError("URL is nil")
+            
+        }
+        NetworkController.performRequestForURL(requestURL, httpMethod: .Put) { (data, error) in
+            self.fetchPosts()
+        }
     }
     
-    func fetchPosts(completion: ((posts: [Post]) -> Void)?) {
-        guard let url = endpoint else {return}
+    func fetchPosts(completion: ((posts: [Post]) -> Void)? = nil) {
+        guard let url = PostController.endpoint else {
+            fatalError("URL optional is nil in \(#file)")
+        }
         
         NetworkController.performRequestForURL(url, httpMethod: .Get) { (data, error) in
             guard let data = data,
@@ -49,7 +67,7 @@ class PostController {
 }
 
 protocol PostControllerDelegate: class {
-    func postUpdated()
+    func postsUpdated()
     
     
 }
